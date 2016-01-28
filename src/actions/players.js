2738da592d62e3b93489d1b42f9d40779a8664da
Ariel Lothlorien <git@arielkauai.com>
2016-01-27 20:20:47 -1000

    Forgot to save this file before the last commit

--- src/actions/players.js
+++ src/actions/players.js
@@ -14,15 +14,29 @@ export function savePlayer(player) {
 		type: types.SAVING,
 		pid: pid
 	})
+	let pss = getState().players[pid] //Player Save State
 	return Promise((resolve, reject) => {
-		let pss = getState().players[pid] //Player Save State
-
 		fs.writeFile(__dirname + '/../../data/players/'+pss.name, JSON.Stringify(pss), (err)=>{
 			if(err) {
 				console.log("Player: Data Save Error...")
 				console.log("[Start of Player Data Dump]"+JSON.Stringify(pss)+"[End of Player Data Dump]")
 				console.log(err)
+				reject("Not Saved")
 			}
+			resolve()
 		})
-	});
+	}).then(()=>{
+		//Success
+		dispatch({
+			type: types.SAVED,
+			pid: pid,
+			data: pss
+		})
+	},()=>{
+		//Failure
+		dispatch({
+			type: types.NOT_SAVED,
+			pid: pid
+		})
+	})
 }
